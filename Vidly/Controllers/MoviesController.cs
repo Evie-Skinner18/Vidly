b6c72c00e22b42e43d33b0e57ad9d1aff4084bcf
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-12-18 16:20:50 +0000

    Added lumen theme for Bootstrap. Got CSS oh no, will fix later

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Random()
+        {
+            var shrek = new Movie() { Name = "Shrek" };
+            return View(shrek);
+        }
+    }
+}
